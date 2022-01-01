45745688aee403e547a0b06b22564aef6e97abbe
Kandy Peter <kandypeter03@gmail.com>
2022-01-01 13:59:50 +0200

    create joke function

--- joke-app/main.js
+++ joke-app/main.js
@@ -1,3 +1,17 @@
-fetch('https://api.blablagues.net/?rub=blagues')
-    .then((res) => res.json())
-    .then((data) => (document.body.textContent = data.data.content.text_head));
+const header = document.querySelector('.header');
+const contents = document.querySelector('.contents');
+
+
+function displayJoke() {
+    fetch('https://api.blablagues.net/?rub=blagues')
+        .then((res) => res.json())
+        .then((data) => {
+            header.textContent = data.data.content.text_head;
+            contents.textContent = 
+                data.data.content.text !== "" ? data.data.content.text : data.data.content.text_hidden;
+        });
+    }
+
+displayJoke();
+
+document.body.addEventListener("click", displayJoke);
