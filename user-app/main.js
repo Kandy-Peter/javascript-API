44fcf27d18ee216b7d09225b8303436866ea528f
Kandy Peter <kandypeter03@gmail.com>
2022-01-29 14:06:26 +0200

    Use fetch with get method

---
+++ user-app/main.js
@@ -0,0 +1,26 @@
+const myHeaders = new Headers();
+const init = {
+  method: "GET",
+  headers: myHeaders,
+  mode: "cors",
+  cache: "default"
+}
+
+// fetch('../data.json', init).then((res) => console.log(res));
+//------------CRUD => create(POST), read(GET), Update(PUT), delete(DELETE)
+
+let init2 = {
+  method: "POST",
+  headers: {
+    "Content-type": "application/json"
+  },
+  body: JSON.stringify({
+    pseudo: "Kandy Peter",
+    message: "hey my team!",
+  }),
+  mode: "cors",
+  credentials: "same-origin",
+}
+document.querySelector('form').addEventListener('submit', () => {
+  fetch("http://localhost:3000/posts", init2).then(() => console.log('data sent'));
+})
