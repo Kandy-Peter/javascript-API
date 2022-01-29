c6477372a6af6cfac9272d1599012db4a7b26afd
Kandy Peter <kandypeter03@gmail.com>
2022-01-29 14:54:16 +0200

    retrive data from the randomUser API

--- user-app/main.js
+++ user-app/main.js
@@ -1,26 +1,19 @@
-const myHeaders = new Headers();
-const init = {
-  method: "GET",
-  headers: myHeaders,
-  mode: "cors",
-  cache: "default"
+let userData = [];
+
+const fecthUser = async () => {
+ await fetch("https://randomuser.me/api/?results=24")
+  .then((res) => res.json())
+  .then((data) => userData = data.results);
+  console.log(userData);
+}
+const renderUsers = async () => {
+  await fecthUser();
+
+  document.body.innerHTML = userData.map((user) =>
+    `
+    <h3>${user.name.first}</h3>
+    `
+  ).join("");
 }
 
-// fetch('../data.json', init).then((res) => console.log(res));
-//------------CRUD => create(POST), read(GET), Update(PUT), delete(DELETE)
-
-let init2 = {
-  method: "POST",
-  headers: {
-    "Content-type": "application/json"
-  },
-  body: JSON.stringify({
-    pseudo: "Kandy Peter",
-    age: 25,
-  }),
-  mode: "cors",
-  credentials: "same-origin",
-}
-document.querySelector('form').addEventListener('submit', () => {
-  fetch("http://localhost:3000/users", init2).then(() => console.log('data sent'));
-})
+renderUsers();
