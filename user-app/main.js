93ae6803ba1cfc6714ab8720b12d260e27555f4c
Kandy Peter <kandypeter03@gmail.com>
2022-01-29 15:11:47 +0200

    change date format and display renderUser

--- user-app/main.js
+++ user-app/main.js
@@ -8,10 +8,23 @@ const fecthUser = async () => {
 }
 const renderUsers = async () => {
   await fecthUser();
+  const formatDate = (date) => {
+    let newDate = new Date(date).toLocaleDateString("en-EN", {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+    })
+    return newDate;
+  }
 
   document.body.innerHTML = userData.map((user) =>
     `
-    <h3>${user.name.first}</h3>
+    <div class='card'>
+      <img src=${user.picture.large} alt=pic of ${user.name.last}" />
+      <h3>${user.name.first}</h3>
+      <p>${user.location.city}, ${formatDate(user.dob.date)}</p>
+      <em>Member since: ${user.registered.date} days</em>
+    </div>
     `
   ).join("");
 }
